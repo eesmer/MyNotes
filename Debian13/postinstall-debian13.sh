6f5bda42c8a589caa2ef30d4fbe8415255159b4f
erkan.esmer <erkanesmer@gmail.com>
2025-09-22 20:41:44 +0300

    Debian13-postinstall script modified

--- Debian13/postinstall-debian13.sh
+++ Debian13/postinstall-debian13.sh
@@ -8,7 +8,6 @@ export DEBIAN_FRONTEND=noninteractive
 HOSTNAME="erkdebian"
 TIMEZONE="Europe/Istanbul"
 MYUSER="erkan"
-UPDATE_REPORT="/tmp/dpkg-conffile-report.$(date +%F_%H%M%S).txt"
 
 hostnamectl set-hostname "$HOSTNAME"
 timedatectl set-timezone "$TIMEZONE"
@@ -53,35 +52,3 @@ EOF
 
 apt-get update && apt-get -y full-upgrade && apt-get -y autoremove --purge && apt-get -y autoclean
 
-echo "[i] Conffile diff scan starting.." | tee "$UPDATE_REPORT"
-printf "NEW_FILE\tORIGINAL\tPACKAGE\tUNITS(owned-by-pkg)\n" | tee -a "$UPDATE_REPORT"
-
-# This block finds packages and services that have not been applied to the confold definition but have been modified in the config file.
-# We check with the following example:
-# diff -u /etc/ssh/sshd_config /etc/ssh/sshd_config.dpkg-dist | less
-
-mapfile -d '' FILES < <(
-  find /etc -type f \
-    \( -name '*.dpkg-dist' -o -name '*.dpkg-new' -o -name '*.dpkg-old' \
-       -o -name '*.ucf-dist' -o -name '*.ucf-new' -o -name '*.ucf-old' \) \
-    -print0 | sort -z
-)
-
-if ((${#FILES[@]}==0)); then
-  echo "[i] No dpkg/ucf conffile copies found." | tee -a "$UPDATE_REPORT"
-else
-  for F in "${FILES[@]}"; do
-    base="${F%.dpkg-dist}"; base="${base%.dpkg-new}"; base="${base%.dpkg-old}"
-    base="${base%.ucf-dist}"; base="${base%.ucf-new}"; base="${base%.ucf-old}"
-
-    pkg="$(dpkg -S "$base" 2>/dev/null | head -n1 | cut -d: -f1 || true)"
-    [[ -z "${pkg:-}" ]] && pkg="(unknown/local)"
-
-    units="$(dpkg -L "$pkg" 2>/dev/null | grep -E '\.(service|socket|timer)$' || true)"
-    units_names="$( [ -n "$units" ] && basename -a $units 2>/dev/null | paste -sd, - || echo "-" )"
-
-    printf "%s\t%s\t%s\t%s\n" "$F" "$base" "$pkg" "$units_names" | tee -a "$UPDATE_REPORT"
-  done
-fi
-
-echo "[i] Report: $UPDATE_REPORT"
