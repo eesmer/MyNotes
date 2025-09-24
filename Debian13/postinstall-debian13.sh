7acd1b6b202c61a67de28a589c5f7d5259649d4d
erkan.esmer <erkanesmer@gmail.com>
2025-09-24 11:07:26 +0300

    Debian13-postinstall script edited

--- Debian13/postinstall-debian13.sh
+++ Debian13/postinstall-debian13.sh
@@ -53,18 +53,18 @@ EOF
 apt-get update && apt-get -y full-upgrade && apt-get -y autoremove --purge && apt-get -y autoclean
 
 # PACKAGES INSTALL
-grep -qi 'GenuineIntel' /proc/cpuinfo && apt-get install -y intel-microcode || grep -qi 'AuthenticAMD' /proc/cpuinfo && apt-get install -y amd64-microcode || true
-apt-get install -y isenkram-cli && isenkram-autoinstall-firmware || true
-apt-get install -y xserver-xorg xserver-xorg-input-libinput xauth
+grep -qi 'GenuineIntel' /proc/cpuinfo && apt-get -y install intel-microcode || grep -qi 'AuthenticAMD' /proc/cpuinfo && apt-get -y install amd64-microcode || true
+apt-get -y install isenkram-cli && isenkram-autoinstall-firmware || true
+apt-get -y install xserver-xorg xserver-xorg-input-libinput xauth
 apt-get -y install i3 i3status xtrlock suckless-tools
-apt-get -y install lxpolkit
 apt-get -y install xterm xinit xfce4-terminal
-apt-get -y install thunar thunar-volman tumbler ffmpegthumbnailer gvfs-backends gvfs-fuse udisks2
 apt-get -y install vim tmux openssh-server htop
-apt-get -y install whiptail
-apt-get -y install x11-xserver-utils
+apt-get -y install sudo
+apt-get -y install lxpolkit
+apt-get -y install x11-xserver-utils whiptail
+apt-get -y install thunar thunar-volman tumbler ffmpegthumbnailer gvfs-backends gvfs-fuse udisks2
 apt-get -y install zsh fzf zsh-autosuggestions zsh-syntax-highlighting ripgrep
-# === MY .zshrc config ===
+# === MY .zshrc CONFIG ===
 cat >"/home/$MYUSER/.zshrc" <<'EOF'
 # ==== MY .zshrc ====
 
