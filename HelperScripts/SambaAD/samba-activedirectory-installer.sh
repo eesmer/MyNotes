9cf0874102e1b6f1d2b1fbb4cd9abb8a9b0272bf
erkan.esmer <erkanesmer@gmail.com>
2025-09-28 22:31:13 +0300

    samba-activedirectory-installer.sh edited

--- HelperScripts/SambaAD/samba-activedirectory-installer.sh
+++ HelperScripts/SambaAD/samba-activedirectory-installer.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------
+# Samba Active Directory Installer
+# - It installs the Samba package and its requirements
+# - It installs and configures bind9 for DNS
+# - It installs and configures the chrony service for the NTP service
+# Then, it performs the Domain Name Provisioning process according to the information it receives and configures the smb.conf file.
+# The machine on which it is run takes the PDC role and starts working as a DC for the established domain.
+# ------------------------------------------------------------------
 # This script has been tested in Debian environment.
-# It is compatible with Debian.
+# It is compatible with Debian
+# It should be run in a Debian 11 or Debian 12 environment.
+# ------------------------------------------------------------------
+# wget https://raw.githubusercontent.com/eesmer/SambaAD-HelperScripts/master/scripts/samba-activedirectory-installer.sh
+# bash samba-activedirectory-installer.sh
 #-------------------------------------------------------------------
 
 # Color Codes
