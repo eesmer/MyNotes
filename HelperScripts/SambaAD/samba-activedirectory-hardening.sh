1353b487793d045b590a8d519186084b98cd3f45
erkan.esmer <erkanesmer@gmail.com>
2025-09-28 22:38:17 +0300

    sambaad hardening script edited

--- HelperScripts/SambaAD/samba-activedirectory-hardening.sh
+++ HelperScripts/SambaAD/samba-activedirectory-hardening.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------------
+# Samba Active Directory Hardening
+# This script, disables or removes non-essential Active Directory settings in Samba configuration.
+# Contains AD related and best practices.
+#------------------------------------------------------------------------------------
+# Please take a backup and trying do not apply without understanding the configuration in each line.
 # This script makes security changes to the Samba Active Directory configuration.
 # Please backup, test and apply.
-#
 # The script must run on the Samba Active Directory Domain Controller.
 #------------------------------------------------------------------------------------
 # This script has been tested only on DebianDC installations and is tailored to DebianDC configurations.
 # DebianDC makes these settings by default.
 # https://github.com/eesmer/DebianDC
+# USAGE:
+# wget https://raw.githubusercontent.com/eesmer/SambaAD-HelperScripts/master/scripts/samba-activedirectory-hardening.sh
+# bash  samba-activedirectory-hardening.sh
 #------------------------------------------------------------------------------------
 
 # Disable printer
