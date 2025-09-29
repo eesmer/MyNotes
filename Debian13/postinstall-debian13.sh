0942827cd79281569a7e3be7d4cedb9bea36defe
erkan.esmer <erkanesmer@gmail.com>
2025-09-29 19:57:08 +0300

    postinstall-debian13.sh edited for install new packages

--- Debian13/postinstall-debian13.sh
+++ Debian13/postinstall-debian13.sh
@@ -82,8 +82,10 @@ apt-get -y install sudo
 apt-get -y install lxpolkit
 apt-get -y install x11-xserver-utils whiptail
 apt-get -y install thunar thunar-volman tumbler ffmpegthumbnailer gvfs-backends gvfs-fuse udisks2
-apt-get -y install zsh fzf zsh-autosuggestions zsh-syntax-highlighting ripgrep
+apt-get -y install zsh fzf zsh-autosuggestions zsh-syntax-highlighting
 apt-get -y install feathernotes atril pavucontrol unzip xfce4-terminal freerdp2-x11 vlc feh xdg-utils desktop-file-utils
+apt-get -y install ripgrep wget curl rsync dnsutils net-tools
+apt-get -y install mtr-tiny traceroute nmap htop lsof tcpdump iperf3 ncdu pv jq ca-certificates
 
 if update-alternatives --list x-terminal-emulator >/dev/null 2>&1; then
   update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper 2>/dev/null || \
