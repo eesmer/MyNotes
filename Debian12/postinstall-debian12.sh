5cac04acf5c2270d648aefc8ffe862f2a1b1486a
erkan.esmer <erkanesmer@gmail.com>
2025-09-29 21:06:47 +0300

    postinstall-debian12.sh script edited

--- Debian12/postinstall-debian12.sh
+++ Debian12/postinstall-debian12.sh
@@ -18,4 +18,6 @@ apt-get -y dist-upgrade
 
 # PACKAGES
 apt-get -y install $(apt search ^firmware- 2> /dev/null | grep ^firmware | grep -v micropython-dl | cut -d "/" -f 1)
-
+apt-get install -y \
+  i3 xtrlock thunar zsh fzf \
+  vim tmux openssh-server htop
