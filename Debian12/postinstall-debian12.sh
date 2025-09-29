7602028cd73ea8dc97037cb0d04fc497e8d63795
erkan.esmer <erkanesmer@gmail.com>
2025-09-29 21:17:30 +0300

    postinstall-debian12.sh script edited

--- Debian12/postinstall-debian12.sh
+++ Debian12/postinstall-debian12.sh
@@ -42,3 +42,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox.gpg] https
 apt-get update -y
 apt-get install -y dkms build-essential linux-headers-$(uname -r)
 apt-get install -y virtualbox-7.1
+
+apt-get -y autoremove
+apt-get clean
+
