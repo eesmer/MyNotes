e1217a01401b6868fe16af45d1951b6d85cd654b
erkan.esmer <erkanesmer@gmail.com>
2025-10-17 22:46:34 +0300

    installer script edited for color code and new variables

--- HelperScripts/SambaAD/samba-activedirectory-installer.sh
+++ HelperScripts/SambaAD/samba-activedirectory-installer.sh
@@ -17,18 +17,21 @@
 # bash samba-activedirectory-installer.sh
 #-------------------------------------------------------------------
 
+#--------------------
 # Color Codes
-MAGENTA="tput setaf 1"
-GREEN="tput setaf 2"
-YELLOW="tput setaf 3"
-DGREEN="tput setaf 4"
-CYAN="tput setaf 6"
-WHITE="tput setaf 7"
-GRAY="tput setaf 8"
-RED="tput setaf 9"
-BLUE="tput setaf 12"
-NOCOL="tput sgr0"
-BOLD="tput bold"
+#--------------------
+MAGENTA=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+WHITE=$(tput setaf 7)
+GRAY=$(tput setaf 8)
+RED=$(tput setaf 9)
+BLUE=$(tput setaf 12)
+NOCOL=$(tput sgr0)
+BOLD=$(tput bold)
+
+MIN_DEBIAN_VER=11
+LOGFILE="/var/log/samba-ad-install.log"
 
 whiptail --msgbox \
         ".:: Samba Active Directory Domain Controller Installer ::. \
